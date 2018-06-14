f061b1dedaaf1e1d1f23c20332dc6c7cc17370ad
Marcel <linusdufe@gmail.com>
2018-06-15 01:29:18 +0200

    CRUD done and commented properly

--- src/crudmongo/MongoExample.java
+++ src/crudmongo/MongoExample.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author yatusabe
  */
 package crudmongo;
-  /*
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
-*/
+
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -20,47 +13,68 @@ import com.mongodb.MongoClient;
 
 public class MongoExample {
     public static void main(String[] args) {
-
+        
+        //Se ejecuta en el localhost en el puerto 27017
         MongoClient mongoClient = new MongoClient("localhost", 27017);
-
+        //Instanciamos la basededatos
         DB database;
-        database = mongoClient.getDB("myMongoDb");
+        //Creamos la bd con el nombre "BaseDatosMongo"
+        database = mongoClient.getDB("BaseDatosMongo");
 
-        // print existing databases
-        mongoClient.getDatabaseNames().forEach(System.out::println);
+        //Mostramos las bases de datos que existen
+        //mongoClient.getDatabaseNames().forEach(System.out::println);
+        //Creamos una coleccion llamada "Biblioteca", dejamos las opciones en null
+        database.createCollection("Biblioteca", null);
 
-        database.createCollection("customers", null);
-
-        // print all collections in customers database
+        //Sacamos por pantallas las collecciones
         database.getCollectionNames().forEach(System.out::println);
 
-        // create data
-        DBCollection collection = database.getCollection("customers");
+        /*Creamos la info*/
+        //Seleccionamos la tabla "Biblioteca"
+        DBCollection collection = database.getCollection("Biblioteca");
+        //instanciamos un objecto de tipo "BasicDBObject" con nombre "document" para insertar info
         BasicDBObject document = new BasicDBObject();
-        document.put("name", "Shubham");
-        document.put("company", "Baeldung");
+        //creamos la info
+        document.put("Título", "Hercule Poirot");
+        document.put("Autor/a", "Agatha Christie");
+        //La insertamos en la coleccion adequada
         collection.insert(document);
 
-        // update data
+        /*Update data*///-->cambiar/actualizar info de la base de datos*/
+        //instanciamos un objeto tipo "BasicDBObject" para decirle qué información queremos cambiar 
         BasicDBObject query = new BasicDBObject();
-        query.put("name", "Shubham");
+        //Datos que se van a cambiar
+        query.put("Título", "Hercule Poirot");
+        //Nueva instancia con los nuevos datos
         BasicDBObject newDocument = new BasicDBObject();
-        newDocument.put("name", "John");
+        //Instroducimos la nueva información
+        newDocument.put("Título", "Asesinato en el orient express");
+        //Efectuamos el cambio(update)
         BasicDBObject updateObject = new BasicDBObject();
         updateObject.put("$set", newDocument);
+        //Cambiamos "newDocument" por "query"
         collection.update(query, updateObject);
 
-        // read data
+        /*Read data*/
+        //instanciamos el mismo objeto que antes para poder hacer consultas
         BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("name", "John");
+        //Le decimos que es lo que queremos buscar
+        searchQuery.put("Título", "Asesinato en el orient express");
+        //Busca
         DBCursor cursor = collection.find(searchQuery);
+        //Recorremos e imprimimnos para ver
         while (cursor.hasNext()) {
             System.out.println(cursor.next());
         }
 
-        // delete data
+        /*Delete data*/
+        //Instanciamos 
         BasicDBObject deleteQuery = new BasicDBObject();
-        deleteQuery.put("name", "John");
+        //Le decimos qué es lo que queremos eliminar
+        deleteQuery.put("Título", "Asesinato en el orient express");
+        //eliminamos
         collection.remove(deleteQuery);
+        //database.dropDatabase();
+        //FIN
     }
 }
